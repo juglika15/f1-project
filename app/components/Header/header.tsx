cbee74aee7a757af6aa4eafcde108e65fa3d9778
juglika <lashajugeli96@gmail.com>
2024-11-18 16:05:03 +0400

    feat(header): add header component

---
+++ app/components/Header/header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul className="flex gap-3 justify-center">
+          <li className="">Home</li>
+          <li>F1 Tickets</li>
+          <li>FAQs</li>
+          <li>Contanct Us</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header/header";
 import Analytics from "./components/Analytics";
 
 const geistSans = localFont({
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
