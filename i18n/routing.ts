69fd7538d0b37c4e1a22e781a0c58b10d79441c6
juglika <lashajugeli96@gmail.com>
2024-12-23 02:41:58 +0400

    feat: add reset password

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -7,17 +7,17 @@ export const routing = defineRouting({
 
   // Used when no locale matches
   defaultLocale: "en",
-  pathnames: {
-    "/": { en: "/", ka: "/" },
-    "/sign-in": { en: "/sign-in", ka: "/შესვლა" },
-    "/sign-up": { en: "/sign-up", ka: "/რეგისტრაცია" },
-    "/forgot-password": { en: "/forgot-password", ka: "/პაროლის-აღდგება" },
-    "/tickets": { en: "/tickets", ka: "/ბილეთები" },
-    "/merchandise": { en: "/merchandise", ka: "/კოლექცია" },
-    "/help": { en: "/help", ka: "/დახმარება" },
-    "/contact": { en: "/contact", ka: "/დაგვიკავშირდი" },
-    "/about": { en: "/about", ka: "/ჩვენს-შესახებ" },
-  },
+  // pathnames: {
+  //   "/": { en: "/", ka: "/" },
+  //   "/sign-in": { en: "/sign-in", ka: "/შესვლა" },
+  //   "/sign-up": { en: "/sign-up", ka: "/რეგისტრაცია" },
+  //   "/forgot-password": { en: "/forgot-password", ka: "/პაროლის-აღდგება" },
+  //   "/tickets": { en: "/tickets", ka: "/ბილეთები" },
+  //   "/merchandise": { en: "/merchandise", ka: "/კოლექცია" },
+  //   "/help": { en: "/help", ka: "/დახმარება" },
+  //   "/contact": { en: "/contact", ka: "/დაგვიკავშირდი" },
+  //   "/about": { en: "/about", ka: "/ჩვენს-შესახებ" },
+  // },
 });
 
 // Lightweight wrappers around Next.js' navigation APIs
