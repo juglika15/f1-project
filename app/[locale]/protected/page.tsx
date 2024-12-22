557f49db6a84414eb80878c3453922f036a77187
juglika <lashajugeli96@gmail.com>
2024-12-23 03:13:18 +0400

    feat: implement protected pages

--- app/[locale]/protected/page.tsx
+++ app/[locale]/protected/page.tsx
@@ -16,7 +16,6 @@ export default async function ProtectedPage() {
     <div className="flex-1 w-full flex flex-col gap-12">
       <div className="w-full">
         <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          {/* <InfoIcon size="16" strokeWidth={2} /> */}
           This is a protected page that you can only see as an authenticated
           user
         </div>
@@ -29,7 +28,6 @@ export default async function ProtectedPage() {
       </div>
       <div>
         <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        {/* <FetchDataSteps /> */}
       </div>
     </div>
   );
