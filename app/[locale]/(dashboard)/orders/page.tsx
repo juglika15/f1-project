14421658ed29a56a78cec990d13fdac767653d91
juglika <lashajugeli96@gmail.com>
2025-01-27 05:22:05 +0400

    #15: add orders page

---
+++ app/[locale]/(dashboard)/orders/page.tsx
@@ -0,0 +1,14 @@
+const Orders = () => {
+  return (
+    <main className="flex flex-grow flex-col justify-center bg-gray-100  dark:bg-dark items-center">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-3xl font-extrabold text-gray-900 dark:text-gray-100">
+            Orders
+          </h2>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Orders;
